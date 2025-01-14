cf03c817e268ca78b3470a978661f7993125bfc9
Axel Huebl <axel.huebl@plasma.ninja>
2025-01-13 17:28:44 -0800

    Sphinx Me Up

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Accelerator Lattice Standard'
+copyright = '2025, under CC-BY 4.0 License'
+author = 'Jean-Luc Vay, David Sagan, Chad Mitchell, Axel Huebl, David Bruhwihler, Christopher Mayes, Eric Stern, Daniel Winklehner, Michael Ehrlichman, Martin Berz, Giovanni Iadarola, Ji Qiang, Edoardo Zoni, Laurent Deniau, et al.'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'sphinx_design']
+myst_enable_extensions = ["colon_fence"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
---
+++ source/index.md
@@ -0,0 +1,21 @@
+# Accelerator Lattice Standard
+
+This standard is an effort to create a standard to promote lattice information exchange for particle accelerators.
+
+```{toctree}
+:maxdepth: 1
+:caption: Front Matter
+
+front_matter/introduction.md
+front_matter/governance.md
+front_matter/contributing.md
+front_matter/how_to_write_docs.md
+```
+
+```{toctree}
+:maxdepth: 1
+:caption: Standard
+
+standard/introduction.md
+standard/element_parameter.md
+```
